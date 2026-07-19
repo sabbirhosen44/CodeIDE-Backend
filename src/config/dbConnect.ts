978e05f368b52c820f4c9936290707e8d952f15d
Md Sabbir Hosen <sabbirhosen44@gmail.com>
2026-07-19 22:41:01 +0600

    Prepare Heroku deployment

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import "colors";
+// import dns from "dns";
+
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export const connectDB = async (): Promise<void> => {
   try {
